7a436fbdfd4b234a14d2306aa463c17c409a4419
Pollybs <pollybernardes@gmail.com>
2023-12-22 11:02:42 +0100

    update_sql

---
+++ questweek3_sql.sql
@@ -0,0 +1,7 @@
+select * from our_new_table
+
+/* SELECT column1, column2,
+       LEAD(column3, 1) OVER (ORDER BY column4) AS next_value,
+       LAG(column3, 1) OVER (ORDER BY column4) AS prev_value
+FROM tablename; */
+
